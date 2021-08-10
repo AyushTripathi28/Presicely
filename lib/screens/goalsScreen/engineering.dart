a02248d4d6f02d02aea8da8a18408b7fadb27767
Satyam Sinha <satyamsinha9404@gmail.com>
2021-08-10 12:54:56 +0530

    added goal engineering screen

---
+++ lib/screens/goalsScreen/engineering.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Engineering extends StatefulWidget {
+  const Engineering({Key? key}) : super(key: key);
+
+  @override
+  _EngineeringState createState() => _EngineeringState();
+}
+
+class _EngineeringState extends State<Engineering> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
